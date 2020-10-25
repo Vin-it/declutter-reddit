0690a96731b06b55d09f8ae1b4ae66e32992dcb5
Vinit <vinit.kumbharkar@pipedrive.com>
2020-10-25 12:12:06 +0200

    DR-1 Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const { PORT } = require('./lib/constants/app');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log('App is listening to', PORT);
+});
