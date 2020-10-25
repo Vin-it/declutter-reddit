3c5b2274327b2e0ad87e85fa9db3e80dc414ed41
Vinit <vinit.kumbharkar@pipedrive.com>
2020-10-25 20:43:25 +0200

    DR-5 Added express session, and session middleware

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 
 const { PORT } = require('./lib/constants/app');
 
+app.use(session({
+  secret: 'fakeSecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  if (!res.locals.isInSession) {
+    res.redirect('/login');
+} else {
+    res.send("Home");
+}
+});
+
+app.get('/login', (req, res) => {
+  res.send('Click here to authorize this app to use your reddit account or if you have already done so please enter your username below');
 });
 
 app.listen(PORT, () => {
