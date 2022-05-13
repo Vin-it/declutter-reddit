c025114dfba318e12951e58175cea364d3bb8a97
Vinit Kumbharkar <vinit.kumbharkar@ramp.network>
2022-05-13 21:09:46 +0200

    fix: webpack get DECLUTTER_ENV from config file
    we were getting wrong variable (ENV_DEFAULT)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const config = require('config');
 
-const ENV_DEFAULT = 'local';
+const { DECLUTTER_ENV, ENV_DEFAULT } = config.get('app');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -12,8 +13,8 @@ module.exports = {
       template: path.join(__dirname, 'views', 'index.ejs'),
     }),
     new webpack.EnvironmentPlugin({
-      DECLUTTER_ENV: ENV_DEFAULT,
-    })
+      DECLUTTER_ENV: DECLUTTER_ENV || ENV_DEFAULT,
+    }),
   ],
   resolve: {
     extensions: ['.jsx', '.js'],
