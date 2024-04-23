c7ee5f1d13c5f7f71623ca4c91a898b1bf921af1
Vinit <vinit1906@gmail.com>
2024-04-24 00:50:47 +0200

    chore: make access_token column 900 length varchar
    I know, I know. It is now a JWT and I don't want to rewrite the
    whole logic to create new columns/store the claims seperately etc.
    
    So I am doing this for now.

--- lib/database/migrations/20201025150211_create_user_table.ts
+++ lib/database/migrations/20201025150211_create_user_table.ts
@@ -5,7 +5,7 @@ exports.up = function (knex: ReturnType<typeof Knex>) {
     table.string('id', 8).primary();
     table.string('username', 255).notNullable().unique();
     table.string('refresh_token', 255).notNullable();
-    table.string('access_token', 255).notNullable();
+    table.string('access_token', 900).notNullable();
     table.timestamp('expires_on').notNullable();
     table.boolean('isImported').notNullable().defaultTo(false);
   });
