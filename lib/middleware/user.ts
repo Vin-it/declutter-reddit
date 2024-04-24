9eb4553f3f3abc74004a080d0f8442f1cdd66893
Vinit <vinit1906@gmail.com>
2024-04-24 20:10:31 +0200

    feat(be): remove leftover accidental random debug console.logs

--- lib/middleware/user.ts
+++ lib/middleware/user.ts
@@ -3,19 +3,15 @@ import { type NextFunction, type Request, type Response } from 'express'
 import { handleExpiredAccessToken } from '../routes/auth/post-login'
 
 async function isInSession (req: Request, res: Response, next: NextFunction) {
-  console.log('IsInSession')
   if (req.session.user) {
-    console.log('if')
     if (new Date(req.session.user.expiresOn) > new Date()) {
       res.locals.isInSession = true
     } else {
-      console.log('else')
       await handleExpiredAccessToken(req.session.user.username)
     }
   } else {
     res.locals.isInSession = false
   }
-  console.log('next will be called', next)
   next()
 }
 
