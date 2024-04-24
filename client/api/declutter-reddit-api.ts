db7cecf76f9cda5f3433d62ab98277a600ffeb83
Vinit <vinit1906@gmail.com>
2024-04-24 08:37:04 +0200

    chore: run eslint --fix on the frontend (client dir)

--- client/api/declutter-reddit-api.ts
+++ client/api/declutter-reddit-api.ts
@@ -1,21 +1,20 @@
 import {
-  BASE_URL,
-} from '../constants/urls';
+  BASE_URL
+} from '../constants/urls'
 
 /* eslint-disable import/prefer-default-export */
 export const getLoggedInUser = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/user`);
+    const response = await fetch(`${BASE_URL}/user`)
 
     if (response.ok) {
-      return response.json();
+      return await response.json()
     }
 
-    throw new Error(`getLoggedInUser, Request failed with ${response.status}`);
+    throw new Error(`getLoggedInUser, Request failed with ${response.status}`)
   } catch (error: unknown) {
-    if (typeof error === 'object' &&  error !== null && 'message' in error) {
-      throw new Error(`getLoggedInUser error ${error.message}`);
+    if (typeof error === 'object' && error !== null && 'message' in error) {
+      throw new Error(`getLoggedInUser error ${error.message}`)
     }
-    
   }
-};
+}
