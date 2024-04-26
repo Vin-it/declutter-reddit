bc124654b4f54713ccdc501ece7b4d7e2a0c0fca
Vinit <vinit1906@gmail.com>
2024-04-26 21:01:59 +0200

    feat: add a sample Home page and a route

--- client/DeclutterRouter.tsx
+++ client/DeclutterRouter.tsx
@@ -1,12 +1,15 @@
 import { Route } from 'wouter';
 import Browse from './components/Browse/Browse';
+import { Home } from './pages/Home/Home';
 export const DeclutterRouter = () => {
   return (
     <>
       <Route path="/browse">
         <Browse />
       </Route>
-      <Route path="/"></Route>
+      <Route path="/">
+        <Home />
+      </Route>
     </>
   );
 };
