6738de5fefba9b999f6e822d5e768a9b7c53b92a
Vinit <vinit1906@gmail.com>
2024-04-27 18:47:02 +0200

    feat(fe): allow toggling between with or without image view in search component

--- client/pages/Search/Search.tsx
+++ client/pages/Search/Search.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { SavedLinks } from '../../utils/reddit-interfaces';
 import { useLocation } from 'wouter';
 import { SearchResultsWithImages } from './SearchResultsWithImges';
+import { SearchResultsWithoutImages } from './SearchResultsWithoutImages';
 
 export const Search = () => {
   const [savedLinks] = useState<SavedLinks>(history?.state?.savedLinks);
+  const [withImages, setWithImages] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
   const [, navigate] = useLocation();
 
@@ -17,82 +19,73 @@ export const Search = () => {
   }
 
   return (
-    <div className="dark:bg-slate-400">
-      <form className="max-w-md mx-auto">
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            onChange={(e) => handleSearchTerm(e.target.value)}
-            type="search"
-            id="default-search"
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search title and subreddit names"
-            required
-          />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <div className="py-3">
+      <div className="flex flex-row justify-center gap-9">
+        <form className="">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
             Search
-          </button>
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              onChange={(e) => handleSearchTerm(e.target.value)}
+              type="search"
+              id="default-search"
+              className="block w-72 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search title and subreddit names"
+              required
+            />
+          </div>
+        </form>
+        <div className="flex items-center">
+          <input
+            checked={withImages}
+            onClick={() => {
+              setWithImages(!withImages);
+            }}
+            id="checked-checkbox"
+            type="checkbox"
+            value=""
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="checked-checkbox"
+            className="ms-2 text-md font-medium"
+          >
+            Checked state
+          </label>
         </div>
-      </form>
-      {/* {listWithoutImages(savedLinks, searchTerm)} */}
-      <SearchResultsWithImages
-        savedLinks={savedLinks}
-        searchTerm={searchTerm}
-      />
+      </div>
+      {withImages ? (
+        <SearchResultsWithImages
+          savedLinks={savedLinks}
+          searchTerm={searchTerm}
+        />
+      ) : (
+        <SearchResultsWithoutImages
+          savedLinks={savedLinks}
+          searchTerm={searchTerm}
+        />
+      )}
     </div>
   );
 };
-
-function listWithoutImages(savedLinks: SavedLinks, searchTerm: string) {
-  return savedLinks?.children
-    .filter(
-      (c) =>
-        Boolean(
-          searchTerm !== '' &&
-            (c?.data?.title?.toLowerCase().indexOf(searchTerm.toLowerCase()) >
-              -1 ||
-              c?.data?.subreddit_name_prefixed
-                ?.toLowerCase()
-                .indexOf(searchTerm.toLowerCase()) > -1)
-        ) || Boolean(searchTerm === '')
-    )
-    .map((c, i) => {
-      return (
-        <div key={i} className="m-3">
-          <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {c.data.subreddit_name_prefixed}
-            </p>
-            <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              <div>{c?.data?.title}</div>
-            </h6>
-          </div>
-        </div>
-      );
-    });
-}
