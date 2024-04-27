5173a390ee46ffbe7b5836cfdb3c20ab5f65c9a7
Vinit <vinit1906@gmail.com>
2024-04-27 19:54:24 +0200

    ref(fe): home page description, and show progress count

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -11,6 +11,7 @@ export const Home = () => {
   const [user, setUser] = useState({
     username: '',
   });
+  const [count, setCount] = useState(0);
   const [savedLinks, setSavedLinks] = useState<SavedLinks | undefined>();
   const handleSearchWithoutImport = async () => {
     const { data: userData } = await getLoggedInUser();
@@ -19,6 +20,7 @@ export const Home = () => {
     tempSavedLinks = response.data;
 
     while (response?.data?.after) {
+      setCount(tempSavedLinks?.children.length ?? 0);
       await delay(700);
       response = await getSavedLinks(userData.user, {
         after: response.data.after,
@@ -36,23 +38,32 @@ export const Home = () => {
       <div className="m-auto">
         <div
           style={{ width: '48rem' }}
-          className="h-30 grid grid-cols-2 divide-x font-mono text-sm text-center font-bold leading-6 rounded-lg shadow-lg overflow-hidden dark:divide-slate-700"
+          className="h-30 grid grid-cols-1 divide-x font-mono text-sm text-center font-bold leading-6 rounded-lg shadow-lg overflow-hidden dark:divide-slate-700"
         >
-          <div className="p-4 text-slate-400 bg-white dark:bg-slate-800">
-            <CustomButton text={'import and search'} />
-            <br />
-            We store the saved links on our server. This way you can come back
-            anytime and have your data ready for searching. You can always
-            delete it with one click.
-          </div>
           <div className="p-4 text-slate-400 bg-white dark:bg-slate-800">
             <CustomButton
               onClick={handleSearchWithoutImport}
-              text={'search without import'}
+              text={'Click here to start'}
             />
             <br />
-            We will not store your saved links. Everytime you visit the website
-            we will fetch it and store it only in the browser.
+            {count === 0 ? (
+              <ul>
+                <li>
+                  - We never save your data on our server. It all stays in your
+                  browser.
+                </li>
+                <li>
+                  - One you leave the search page you will have to start again.
+                  (But you can refresh it)
+                </li>
+                <li>
+                  - You can report bugs or request features at -
+                  decluttereddit@gmail.com
+                </li>
+              </ul>
+            ) : (
+              `Loaded ${count} results...`
+            )}
           </div>
         </div>
       </div>
