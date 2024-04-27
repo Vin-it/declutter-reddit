7b942724056929a5c42d1e46f84ad4b23df55135
Vinit <vinit1906@gmail.com>
2024-04-27 18:42:49 +0200

    feat(fe): add search without images component

---
+++ client/pages/Search/SearchResultsWithoutImages.tsx
@@ -0,0 +1,32 @@
+import { renderImage } from '../../components/SavedLink/SavedLink';
+import { searchSavedLinkChild } from '../../utils/general.utils';
+import { SavedLinks } from '../../utils/reddit-interfaces';
+
+export const SearchResultsWithoutImages = ({
+  savedLinks,
+  searchTerm,
+}: {
+  savedLinks: SavedLinks;
+  searchTerm: string;
+}) => {
+  return (
+    <>
+      {savedLinks?.children
+        .filter((c) => searchSavedLinkChild(c, searchTerm))
+        .map((c, i) => {
+          return (
+            <div key={i} className="m-3">
+              <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {c.data.subreddit_name_prefixed}
+                </p>
+                <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <div>{c?.data?.title}</div>
+                </h6>
+              </div>
+            </div>
+          );
+        })}
+    </>
+  );
+};
