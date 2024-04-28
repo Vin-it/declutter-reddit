9e8ff0fac7acb71bf995bec26ae6b0e46acbb952
Vinit <vinit1906@gmail.com>
2024-04-28 12:51:13 +0200

    feat(be): add endpoint to send saved posts fetched from reddit

--- lib/router.ts
+++ lib/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRouter from './routes/auth';
+import { redditRouter } from './routes/reddit/reddit.router';
 
 const mainRouter = express.Router();
 
 mainRouter.use(authRouter);
+mainRouter.use(redditRouter);
 mainRouter.get('*', (req, res) => {
   res.render('index');
 });
