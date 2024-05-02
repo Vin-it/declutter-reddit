13a889e6aec29cc64fa9dd7cac5e1148fc561624
Vinit <vinit1906@gmail.com>
2024-05-02 20:09:34 +0200

    chore(fe): include .ejs in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./client/**/*.{html,tsx}"],
+  content: ["./client/**/*.{html,tsx,ejs}"],
   theme: {
     extend: {},
   },
